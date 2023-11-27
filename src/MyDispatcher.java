06a0cd5cb7fb73913488df79545281fac1082d20
Vlad Iliescu <vladtoma.iliescu@gmail.com>
2023-11-27 10:22:19 +0200

    Added Schelet

---
+++ src/MyDispatcher.java
@@ -0,0 +1,14 @@
+/* Implement this class. */
+
+import java.util.List;
+
+public class MyDispatcher extends Dispatcher {
+
+    public MyDispatcher(SchedulingAlgorithm algorithm, List<Host> hosts) {
+        super(algorithm, hosts);
+    }
+
+    @Override
+    public void addTask(Task task) {
+    }
+}
---
+++ src/MyHost.java
@@ -0,0 +1,25 @@
+/* Implement this class. */
+
+public class MyHost extends Host {
+    @Override
+    public void run() {
+    }
+
+    @Override
+    public void addTask(Task task) {
+    }
+
+    @Override
+    public int getQueueSize() {
+        return 0;
+    }
+
+    @Override
+    public long getWorkLeft() {
+        return 0;
+    }
+
+    @Override
+    public void shutdown() {
+    }
+}
